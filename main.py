410a99e8d532b667368a35de0f91d1d6277f8933
ntargono082008 <nathanbechill@gmail.com>
2025-11-05 23:10:02 +0000

    Added user selection hud

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-print("Welcome to the Amazon Chatbot")
+print("Welcome to the H&M Chatbot")
 
-name = input("What is your name? ")
-age = input(f"Hello {name} is your age? ")
+def chatbot_greeting():
+    name = input("What is your name? ")
+    age = input(f"Hello {name} what is your age? ")
+    print(f"Great to here that even at {age} you still choose to shop at H&M")
+
+def display_menu():
+    print("1. Placeholder")
+    print("2. Placeholder")
+    print("3. Placeholder")
+    print("4. Exit")
+
+def user_selection():
+    in_use = True
+    user_input = input("\nEnter a number between 1-4: ")
+    if user_input == "1":
+        print("1")
+    elif user_input == "2":
+        print("2")
+    elif user_input == "3":
+        print("3")
+    elif user_input == "4":
+        print("Exit program")
+        in_use = False
+    else:
+        print("Invalid option: please enter a number between 1-4")
+    return in_use
+
+
+
+chatbot_greeting()
+display_menu()
+user_selection()
