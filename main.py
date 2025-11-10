adef00cc94fad88d7deed57d966520766c074093
ntargono082008 <nathanbechill@gmail.com>
2025-11-10 23:16:25 +0000

    Added Option 3

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main_menu():
     print("-------------------------------------")
     print("1. Rate a Purchase")
     print("2. Write a Review")
-    print("3. Placeholder")
+    print("3. Customer Support")
     print("4. Exit")
     user_input = input("\nEnter a number between 1-4: ")
     if user_input == "1":
@@ -30,7 +30,7 @@ def main_menu():
     elif user_input == "2":
         product_review()
     elif user_input == "3":
-        print("3")
+        customer_support()
     elif user_input == "4":
         print("Aw sorry you had to go. Goodbye!")
         in_use = False
@@ -83,8 +83,45 @@ def product_review():
     print("Thank you for sharing your review.")
     return_main_menu()
     
+def customer_support():
+    in_use = True
+    print("What appears to be your issue?")
+    print("-------------------------------------")
+    print("1. Return a Product")
+    print("2. Shipping Issues")
+    print("3. Return to the Main Menu")
+    user_input = input("\nEnter a number between 1-3: ")
+    if user_input == "1":
+       print("Now taking you to the (Return a Product Line)")
+       return_product()
+    elif user_input == "2":
+        print("Now taking you to the (Shipping Issues line)")
+        shipping_issues()
+    elif user_input == "3":
+        print("Now returning to the Main Menu")
+        main_menu()
+    else:
+        print("Invalid option: please enter a number between 1-3")
+    return in_use
 
-
+def return_product():
+    while True:
+        user_product = input("What would you like to return? ")
+        user_clar = input(f"You want to return {user_product}. Is this correct? (YES/NO) ")
+        if user_clar.lower() == "yes":
+            print("Great! Please head to your local H&MM with your receipt ready. Thank you!")
+            break
+        elif user_clar.lower() == "no":
+            print("Please enter the correct product you wish to return.")
+        else:
+            print("Invalid input. Please enter YES/NO")
+    
+def shipping_issues():
+    user_shipping = input("What appears to be the issue involving shipping? ")
+    print("We apologize to hear that. We will look into it and let you know why this is the case in a later date.")
+    user_contactinfo = input("Please enter a method we can contact you. (Email/Phone Number) ")
+    print(f"Thank you! We will let you know via {user_contactinfo} very soon.")
+    
 
 chatbot_greeting()
 main_menu()
